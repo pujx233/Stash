b780e17ccd5e5cc95a52c06bd7d9d9ec47f02c90
pujx233 <1075885060@qq.com>
2024-04-21 17:23:01 +0800

    Fix: 修复屏蔽不生效的bug

--- reject-tachimanga-requests.js
+++ reject-tachimanga-requests.js
@@ -1,10 +1,8 @@
 function onRequest(context) {
     var userAgent = context.request.headers['User-Agent'];
     if (userAgent && userAgent.includes('Tachimanga')) {
-        // 拒绝请求
         context.reject();
     } else {
-        // 允许请求继续
         context.next();
     }
 }
