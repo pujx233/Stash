b440453f5b633f492ca699b7808ff00db8c1ae7b
pujx233 <1075885060@qq.com>
2024-04-26 00:03:27 +0800

    Performance: 优化脚本格式

--- reject-tachimanga-requests.js
+++ reject-tachimanga-requests.js
@@ -1,14 +1,8 @@
 const targetUserAgent = 'Tachimanga';
 
-if ($request && $request.headers && $request.headers['User-Agent']) {
-    let userAgent = $request.headers['User-Agent'];
-    if (userAgent.includes(targetUserAgent)) {
-        console.log("Blocking request with User-Agent containing 'Tachimanga'");
-        $done({ response: { status: 403, body: 'Access denied' } });
-    } else {
-        console.log("Request allowed with User-Agent: " + userAgent);
-        $done({});
-    }
+if ($request?.headers?.['User-Agent']?.includes(targetUserAgent)) {
+    console.log("Blocking request with User-Agent containing 'Tachimanga");
+    $done({ response: { status: 403, body: 'Access denied' } });
 } else {
     $done({});
 }
